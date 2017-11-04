c2f47fdb6bfcefdfe45d96947c0386874193958e
Nina <Nina@192.168.1.201>
2017-11-04 10:52:23 +0800

    1.0

---
+++ xhWeb/src/main/java/com/cssnb/baseframework/quickstart/entity/admin/User.java
@@ -0,0 +1,91 @@
+package com.cssnb.baseframework.quickstart.entity.admin;
+
+import java.io.Serializable;
+
+/**
+ * Created by HuYing on 2015/1/27.
+ */
+public class User implements Serializable{
+
+    public Integer userId;
+    private String userName;
+    private String nickName;
+    private String password;
+    private String sex;
+    private String phone;
+    private String address;
+    private String lastLoginTime;
+    private String lastLoginIp;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLastLoginTime() {
+        return lastLoginTime;
+    }
+
+    public void setLastLoginTime(String lastLoginTime) {
+        this.lastLoginTime = lastLoginTime;
+    }
+
+    public String getLastLoginIp() {
+        return lastLoginIp;
+    }
+
+    public void setLastLoginIp(String lastLoginIp) {
+        this.lastLoginIp = lastLoginIp;
+    }
+}
